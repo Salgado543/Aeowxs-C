db34501d0642c09b9e03f829836504500d1d013b
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-07-24 19:32:18 -0500

    Actualizar buscador-spotifysearch.js

--- plugins/buscador-spotifysearch.js
+++ plugins/buscador-spotifysearch.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 const { generateWAMessageContent, generateWAMessageFromContent, proto } = (await import('@whiskeysockets/baileys')).default
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) return m.reply(`*${emojis} Ingresa el texto de lo que quieres buscar en Spotify.*`);
+if (!text) return await conn.reply(m.chat, `*${emojis} Ingresa el texto de lo que quieres buscar en Spotify.*`, m, rcanal);
 await m.react('🕓');
 
 try {
