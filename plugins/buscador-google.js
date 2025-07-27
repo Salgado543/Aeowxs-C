e0754ce1393a8c6062bdcdc9c8d7bf79d99911ec
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-07-27 14:17:52 -0500

    Actualizar buscador-google.js

--- plugins/buscador-google.js
+++ plugins/buscador-google.js
@@ -19,3 +19,31 @@ Mi número es +${creatorNumber} si lo necesitas`;
 handler.command = /^hola$/i;
 
 export default handler;*/
+
+const handler = async (m, { conn, args }) => {
+  const targetUser = m.sender;
+  const creatorNumber = '51927238856';
+  const creatorMention = '@' + creatorNumber;
+  const userMention = '@' + targetUser.split('@')[0];
+
+  const text = `Hola soy shadow ${args[0] || userMention}
+
+Mi creador es ${creatorMention}
+
+Mi número es +${creatorNumber} si lo necesitas`;
+
+  await conn.sendMessage(m.chat, {
+    text,
+    footer: 'Shadow Bot',
+    buttons: [
+      { buttonId: '.hola', buttonText: { displayText: '👋 Hola' }, type: 1 },
+      { buttonId: '.chao', buttonText: { displayText: '👋 Chao' }, type: 1 }
+    ],
+    headerType: 1,
+    mentions: [targetUser, `${creatorNumber}@s.whatsapp.net`]
+  }, { quoted: m });
+};
+
+handler.command = /^hola$/i;
+
+export default handler;
