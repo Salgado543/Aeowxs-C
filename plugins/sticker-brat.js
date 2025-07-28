6963b89c6be2330819d5158205cfe6b393b69703
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-07-27 22:45:59 -0500

    Actualizar sticker-brat.js

--- plugins/sticker-brat.js
+++ plugins/sticker-brat.js
@@ -3,10 +3,8 @@ import { sticker } from '../lib/sticker.js'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
   try {
     if (!text) {
-      return conn.reply(m.chat, `*${xsticker} Por favor, ingresa un texto para realizar tu sticker.*`, m, rcanal)
+      return conn.reply(m.chat, `*${emojis} Por favor, ingresa un texto para realizar tu sticker.*`, m, rcanal)
     }
-
-    await m.react('⚡')
 
     const url = `https://api.nekorinn.my.id/maker/brat-v2?text=${encodeURIComponent(text)}`
     const stiker = await sticker(null, url, packname, author)
