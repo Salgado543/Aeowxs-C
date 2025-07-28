94dcf9a697b7931cd813dc1047777892d868dc63
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-07-28 15:54:59 -0500

    Actualizar owner-unbanuser.js

--- plugins/owner-unbanuser.js
+++ plugins/owner-unbanuser.js
@@ -1,4 +1,4 @@
-const handler = async (m, { conn, args, text, usedPrefix, command }) => {
+/*const handler = async (m, { conn, args, text, usedPrefix, command }) => {
     let user;
     let db = global.db.data.users;
     if (m.quoted) {
@@ -20,6 +20,42 @@ const handler = async (m, { conn, args, text, usedPrefix, command }) => {
         await conn.reply(m.chat, `*⚠️ El usuario no está registrado.*`, m);
     }
 };
+handler.help = ['unbanuser'];
+handler.command = ['unbanuser'];
+handler.tags = ['owner'];
+handler.rowner = true;
+
+export default handler;*/
+
+const handler = async (m, { conn }) => {
+    let user;
+    const db = global.db.data.users;
+
+    // Obtener JID del usuario a desbanear
+    if (m.quoted) {
+        user = m.quoted.sender;
+    } else if (m.mentionedJid && m.mentionedJid.length) {
+        user = m.mentionedJid[0];
+    } else {
+        await conn.reply(m.chat, `*${emojis} Etiqueta o responde al usuario que deseas desbanear.*`, m, rcanal);
+        return;
+    }
+
+    if (db[user]) {
+        db[user].banned = false;
+        db[user].banReason = '';
+        db[user].bannedBy = null;
+
+        const nametag = await conn.getName(user);
+        await conn.reply(m.chat, `*${done} El usuario* *${nametag}* *ha sido desbaneado.*`, m, {
+            mentions: [user]
+        });
+
+    } else {
+        await conn.reply(m.chat, `*⚠️ El usuario no está registrado.*`, m);
+    }
+};
+
 handler.help = ['unbanuser'];
 handler.command = ['unbanuser'];
 handler.tags = ['owner'];
