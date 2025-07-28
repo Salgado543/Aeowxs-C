83f1f7bffba8709a5343a0dff8b67bb4e9bc591a
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-07-27 22:49:32 -0500

    Actualizar owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -2,18 +2,18 @@
 import { execSync } from 'child_process'
 
 let handler = async (m, { conn, text }) => {
-  await m.react('🕓') // Reacción inicial
+  await m.react('👨🏻‍💻')
 
   try {
     let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : '')).toString().trim() // Ejecuta git pull y limpia espacios
     let mensaje = stdout.includes('Already up to date') 
-      ? '*☁️ El repositorio del bot está actualizado.*' 
-      : '*☕ \`Repositorio Fixed\`*\n\n' + stdout
+      ? '*⚪ El repositorio del bot está actualizado.*' 
+      : '*Owner - Update*\n\n' + stdout
 
     await conn.reply(m.chat, mensaje, m) // Envía el mensaje con el resultado
-    await m.react('✅') // Reacción de éxito
+    await m.react('☕')
   } catch (err) {
-    await conn.reply(m.chat, `❌ Error al actualizar:\n${err.message}`, m) // Manejo de error
+    await conn.reply(m.chat, `❌ Error al actualizar:\n${err.message}`, m)
   }
 }
 
