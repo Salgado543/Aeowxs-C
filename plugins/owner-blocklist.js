dbf09486f4db04fe72b3482ee8561bb8b8ffdf3d
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-07-28 11:00:44 -0500

    Actualizar owner-blocklist.js

--- plugins/owner-blocklist.js
+++ plugins/owner-blocklist.js
@@ -1,25 +1,20 @@
 
-/* Creado por https://github.com/ianalejandrook15x
-Mejorado por Dev Criss */
-
-const handler = async (m, { conn }) => {
-  try {
-    const blocked = await conn.fetchBlocklist();
-    if (!blocked || !blocked.length) await conn.reply(m.chat, 'No hay números bloqueados.',m);
-
-    let list = blocked.map((jid, index) => `▢ @${jid.split('@')[0]}`).join('\n');
-    let txt = `*≡ Lista de bloqueados*\n\n*Total :* ${blocked.length}\n\n┌─⊷\n${list}\n└───────────`;
-
-    await conn.reply(m.chat, txt, m, { mentions: blocked });
-  } catch (err) {
-    console.error(err);
-    throw typeof err === 'string' ? err : 'Ocurrió un error al obtener la lista de bloqueados.';
-  }
+const handler = async (m, {conn}) => {
+await conn.fetchBlocklist().then(async (data) => {
+let txt = *≡ Lista de bloqueados*\n\n*Total :* ${data.length}\n\n┌─⊷\n;
+for (const i of data) {
+txt += ▢ @${i.split('@')[0]}\n;
+}
+txt += '└───────────';
+return conn.reply(m.chat, txt, m, {mentions: await conn.parseMention(txt)});
+}).catch((err) => {
+console.log(err);
+throw 'No hay números bloqueados';
+});
 };
-
 handler.help = ['blocklist'];
 handler.tags = ['owner'];
 handler.command = ['blocklist', 'listblock'];
 handler.rowner = true;
-
 export default handler;
+
