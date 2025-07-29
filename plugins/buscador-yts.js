671840b1be6fef5c4a71db8889cbdca9e9b5a1ae
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-07-29 12:01:29 -0500

    Actualizar buscador-yts.js

--- plugins/buscador-yts.js
+++ plugins/buscador-yts.js
@@ -1,6 +1,5 @@
-/*
-import yts from 'yt-search'
 
+import yts from 'yt-search'
 
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 
@@ -22,55 +21,12 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
 `.trim()).join('\n________________________\n\n')
 
   let teks = `*Search - Youtube*\n\n${ms}`
-  teks += `\n\n> ${club}`
+  teks += `\n\n> ${dev}`
 
   conn.sendFile(m.chat, tes[0].image, 'yts.jpeg', teks, m)
 }
 
 handler.help = ['yts2'] 
-handler.tags = ['search']
-handler.command = ['ytsearch', 'yts']
-
-export default handler*/
-
-import yts from 'yt-search'
-
-const emojis = '🎵';
-const club = 'Shadow Bot';
-
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) {
-    return conn.reply(m.chat, `*${emojis} Ingresa un texto para buscar en Youtube.*\n> *Ejemplo:* .${command} Coqueta - Grupo Frontera`, m);
-  }
-
-  try { await m.react('🔎') } catch (e) {}
-
-  let results
-  try {
-    results = await yts(text)
-  } catch (e) {
-    console.error(e)
-    return conn.reply(m.chat, '❌ Error al buscar en YouTube.', m)
-  }
-
-  let tes = results.videos
-  if (!tes.length) return conn.reply(m.chat, '⚠️ No se encontraron resultados.', m)
-
-  let ms = tes.map(v => `
-° ${v.title}
-⏰ *Duración:* ${v.timestamp}
-☁️ *Publicado:* ${v.ago}
-👀 *Vistas:* ${v.views.toLocaleString()}
-⛓️ *Enlace:* ${v.url}
-`.trim()).join('\n________________________\n\n')
-
-  let teks = `*🔎 Resultados de búsqueda en YouTube:*\n\n${ms}`
-  teks += `\n\n> ${club}`
-
-  conn.sendFile(m.chat, tes[0].image, 'yts.jpeg', teks, m)
-}
-
-handler.help = ['ytsearch']
 handler.tags = ['search']
 handler.command = ['ytsearch', 'yts']
 
