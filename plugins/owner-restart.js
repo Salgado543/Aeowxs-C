c5fd9a18c6e01f2c0b0c0b7f48018567b22a5d8f
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-07-29 11:04:40 -0500

    Actualizar owner-restart.js

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -1,4 +1,4 @@
-
+/*
 const handler = async (m, { conn }) => {
   try {
     await m.reply('「❀」 Reiniciando el bot...\n\n⏳ Espere unos segundos.');
@@ -14,4 +14,16 @@ handler.tags = ['owner'];
 handler.command = ['restart', 'reiniciar'];
 handler.rowner = true;
 
+export default handler;*/
+
+const handler = async (m, {conn, isROwner, text}) => {
+  if (!process.send) throw 'Dont: node main.js\nDo: node index.js';
+    // conn.readMessages([m.key])
+    await m.reply('*❤️‍🩹 Reiniciando el bot...*\n\n*» Espere un momento para volver a usar el Bot, puede tomar unos segundos.*');
+    process.send('reset');
+};
+handler.help = ['restart'];
+handler.tags = ['owner'];
+handler.command = ['restart', 'reiniciar'];
+handler.rowner = true;
 export default handler;
