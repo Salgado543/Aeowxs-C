a6b2937ee2866bc13cc5e68f9020137958d5ab7e
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-07-29 11:41:31 -0500

    Actualizar buscador-yts.js

--- plugins/buscador-yts.js
+++ plugins/buscador-yts.js
@@ -3,7 +3,7 @@ import yts from 'yt-search'
 
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 
-  if (!text) return conn.reply(m.chat, `*${xsearch} Por favor, ingresa un texto para buscar en Youtube.*\n> *\`Ejemplo:\`* .${command} Amorfoda`, m);
+  if (!text) return conn.reply(m.chat, `*${xsearch} Por favor, ingresa un texto para buscar en Youtube.*\n> *\`Ejemplo:\`* .${command} Shadow's Club`, m, rcanal);
 
  await m.react('🔎');
   let results = await yts(text)
