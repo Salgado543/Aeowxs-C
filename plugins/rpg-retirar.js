0d3a74927cee904e8df6f08fe21e1aa26ecd5e9a
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-07-31 15:46:38 -0500

    Actualizar rpg-retirar.js

--- plugins/rpg-retirar.js
+++ plugins/rpg-retirar.js
@@ -6,16 +6,16 @@ if (!args[0]) return m.reply(`*${emoji} Ingresa la cantidad de coins que deseas 
 if (args[0] == 'all') {
 let count = parseInt(user.bank)
 user.bank -= count * 1
-user.coin += count * 1
+user.coins += count * 1
 await m.reply(`*${emoji} Retiraste ${count} ${moneda} del banco, ahora podras usarlo pero también podrán robartelo.*`)
 return !0
 }
 if (!Number(args[0])) return m.reply(`*⚠️ Debes retirar una cantidad válida.*`)
 let count = parseInt(args[0])
 if (!user.bank) return m.reply(`*✖️ No tienes suficientes ${moneda} en el Banco.*`)
-if (user.bank < count) return m.reply(`*⚠️ Solo tienes ${user.bank} ${moneda} en el Banco.*`)
+if (user.bank < count) return m.reply(`*🪙 Solo tienes ${user.bank} ${moneda} en el Banco.*`)
 user.bank -= count * 1
-user.coin += count * 1
+user.coins += count * 1
 await m.reply(`*${emojis} Retiraste ${count} ${moneda} del banco, ahora podras usarlo pero también podrán robartelo.*`)}
 
 handler.help = ['retirar']
