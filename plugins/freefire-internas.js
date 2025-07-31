66a0e9005fd4909ff66b64767f86bfe29f07f65f
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-07-31 18:36:06 -0500

    Actualizar freefire-internas.js

--- plugins/freefire-internas.js
+++ plugins/freefire-internas.js
@@ -10,7 +10,15 @@ const handler = async (m, { text, conn, args, usedPrefix, command }) => {
     // Nueva validación para formato de 24 horas
     const horaRegex = /^([01]?[0-9]|2[0-3])(:[0-5][0-9])?$/;  
     if (!horaRegex.test(args[0])) {  
-        conn.reply(m.chat, '*⏰ El formato horario es incorrecto.*', m);  
+        conn.reply(m.chat, '*⏰ El formato horario es incorrecto.*\n*Usos horarios:*
+- MX
+- CO
+- EC
+- PE
+- VE
+- DO
+- CL
+- AR', m);  
         return;  
     }  
 
