a6dc3360168369dd896518c0c2222ab3c6179f36
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-07-31 15:34:52 -0500

    Actualizar rpg-depositar.js

--- plugins/rpg-depositar.js
+++ plugins/rpg-depositar.js
@@ -5,17 +5,17 @@ let user = global.db.data.users[m.sender]
 if (!args[0]) return m.reply(`*${emojis} Ingresa la cantidad de coins que deseas depositar en el banco.*`)
 if ((args[0]) < 1) return m.reply('*⚠️ Ingresa una cantidad válida de coins que tienes.*')
 if (args[0] == 'all') {
-let count = parseInt(user.diamantes)
-user.diamantes -= count * 1
+let count = parseInt(user.coins)
+user.coins -= count * 1
 user.bank += count * 1
 await m.reply(`*🏦 Depositaste \`${count}\` ${moneda} al Banco.*`)
 return !0
 }
 if (!Number(args[0])) return m.reply('*⚠️ La cantidad deve ser un número.*')
 let count = parseInt(args[0])
-if (!user.diamantes) return m.reply('*✖️ No tienes ${moneda} en tu Cartera.*')
-if (user.diamantes < count) return m.reply(`*💎 Solo tienes ${user.diamantes} ${moneda} en tu Cartera.*`)
-user.diamantes -= count * 1
+if (!user.coins) return m.reply('*✖️ No tienes ${moneda} en tu Cartera.*')
+if (user.coins < count) return m.reply(`*💎 Solo tienes ${user.coins} ${moneda} en tu Cartera.*`)
+user.coins -= count * 1
 user.bank += count * 1
 await m.reply(`*🏦 Depositaste \`${count}\` ${moneda} al Banco.*`)}
 
