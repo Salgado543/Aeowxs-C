5aa1cd8311644ee07bc2b884931691eaa2aa5e1b
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-07-31 15:07:35 -0500

    Actualizar rpg-bank.js

--- plugins/rpg-bank.js
+++ plugins/rpg-bank.js
@@ -3,32 +3,20 @@ import db from '../lib/database.js'
 const img = 'https://files.catbox.moe/zggh6y.jpg'
 
 const fkontak2 = {
-    key: {
-      participants: "0@s.whatsapp.net",
-      remoteJid: "status@broadcast",
-      fromMe: false,
-      id: "Halo"
-    },
-    message: {
-      locationMessage: {
-        name: `Banco - ${botname}`,
-        jpegThumbnail: await (await fetch('https://files.catbox.moe/a4a2yz.png')).buffer(),
-        vcard:
-          "BEGIN:VCARD\n" +
-          "VERSION:3.0\n" +
-          "N:;Unlimited;;;\n" +
-          "FN:Unlimited\n" +
-          "ORG:Unlimited\n" +
-          "TITLE:\n" +
-          "item1.TEL;waid=19709001746:+1 (970) 900-1746\n" +
-          "item1.X-ABLabel:Unlimited\n" +
-          "X-WA-BIZ-DESCRIPTION:ofc\n" +
-          "X-WA-BIZ-NAME:Unlimited\n" +
-          "END:VCARD"
-      }
-    },
-    participant: "0@s.whatsapp.net"
-  };
+  key: {
+    participants: "0@s.whatsapp.net",
+    remoteJid: "status@broadcast",
+    fromMe: false,
+    id: "Halo"
+  },
+  message: {
+    locationMessage: {
+      name: `Banco - ${botname}`,
+      jpegThumbnail: await (await fetch('https://files.catbox.moe/a4a2yz.png')).buffer()
+    }
+  },
+  participant: "0@s.whatsapp.net"
+};
 
 let handler = async (m, { conn, usedPrefix }) => {
   const who = m.mentionedJid?.[0] || m.quoted?.sender || m.sender
