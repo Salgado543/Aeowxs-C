74ac767afb778974197009e8fd72d7d6c00d1d8a
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-08-01 15:34:48 -0500

    Actualizar owner-revall.js

--- plugins/owner-revall.js
+++ plugins/owner-revall.js
@@ -1,4 +1,4 @@
-/*
+
 import fs from 'fs';
 import path from 'path';
 
@@ -59,80 +59,4 @@ handler.help = ['revsall', 'revall', 'allrev'];
 handler.tags = ['owner'];
 handler.owner = true;
 
-export default handler;*/
-
-import fs from 'fs/promises'
-import path from 'path'
-import vm from 'vm'
-import { fileURLToPath, pathToFileURL } from 'url'
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
-
-const handler = async (m, { conn }) => {
-  const ignoredFolders = ['node_modules', '.git', 'sessions', 'media', 'tmp', 'temp']
-  const ignoredFiles = ['package-lock.json']
-
-  async function getAllJSFiles(dir) {
-    let jsFiles = []
-    const items = await fs.readdir(dir, { withFileTypes: true })
-
-    for (const item of items) {
-      const fullPath = path.join(dir, item.name)
-      if (ignoredFolders.includes(item.name) || ignoredFiles.includes(item.name)) continue
-
-      if (item.isDirectory()) {
-        const subFiles = await getAllJSFiles(fullPath)
-        jsFiles = jsFiles.concat(subFiles)
-      } else if (item.isFile() && fullPath.endsWith('.js')) {
-        jsFiles.push(fullPath)
-      }
-    }
-
-    return jsFiles
-  }
-
-  async function checkSyntaxOnly(filePath) {
-    try {
-      const code = await fs.readFile(filePath, 'utf-8')
-      new vm.Script(code, { filename: filePath }) // Solo revisa sintaxis
-      return null
-    } catch (err) {
-      return err
-    }
-  }
-
-  try {
-    await m.react('🇦🇱')
-    conn.sendPresenceUpdate('composing', m.chat)
-
-    const baseDir = path.resolve('./')
-    const jsFiles = await getAllJSFiles(baseDir)
-
-    let response = `🖤 *Revisión de Syntax Errors en ${jsFiles.length} archivos:*\n\n`
-    let hasErrors = false
-
-    for (const file of jsFiles) {
-      const error = await checkSyntaxOnly(file)
-      if (error) {
-        hasErrors = true
-        response += `🗣️ *Error en:* ${file.replace(baseDir + '/', '')}\n📌 *Tipo:* ${error.name}\n📄 *Mensaje:* ${error.message}\n\n`
-      }
-    }
-
-    if (!hasErrors) {
-      response += '🪐 ¡Todo está en orden! No se detectaron errores de sintaxis.'
-    }
-
-    await conn.reply(m.chat, response, m)
-    await m.react('✅')
-  } catch (err) {
-    await conn.reply(m.chat, `*Error:* ${err.message}`, m)
-  }
-}
-
-handler.command = ['revsall']
-handler.help = ['revsall', 'revall', 'allrev']
-handler.tags = ['owner']
-handler.owner = true
-
-export default handler
+export default handler;
