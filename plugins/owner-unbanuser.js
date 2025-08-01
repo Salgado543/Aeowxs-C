2333f04ae6321e6f767dd745cfccb0c7fca4833a
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-08-01 13:44:28 -0500

    Actualizar owner-unbanuser.js

--- plugins/owner-unbanuser.js
+++ plugins/owner-unbanuser.js
@@ -1,31 +1,3 @@
-/*const handler = async (m, { conn, args, text, usedPrefix, command }) => {
-    let user;
-    let db = global.db.data.users;
-    if (m.quoted) {
-        user = m.quoted.sender;
-    } else if (args.length >= 1) {
-        user = args[0].replace('@', '') + '@s.whatsapp.net';
-    } else {
-        await conn.reply(m.chat, `*${emojis} Etiqueta o coloca el número del usuario que quieres desbanear.*`, m, rcanal);
-        return;
-    }
-    if (db[user]) {
-        db[user].banned = false;
-        db[user].banRazon = '';
-        const nametag = await conn.getName(user);
-        const nn = conn.getName(m.sender);
-        await conn.reply(m.chat, `*${done} El usuario* *${nametag}* *ha sido desbaneado.*`, m, { mentionedJid: [user] });
-
-    } else {
-        await conn.reply(m.chat, `*⚠️ El usuario no está registrado.*`, m);
-    }
-};
-handler.help = ['unbanuser'];
-handler.command = ['unbanuser'];
-handler.tags = ['owner'];
-handler.rowner = true;
-
-export default handler;*/
 
 const handler = async (m, { conn }) => {
     let user;
