9c1b8f1c234bf258c7f1dac2ebb09b2169714ac1
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-08-02 11:16:39 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/*import { join, dirname } from 'path';
+import { join, dirname } from 'path';
 import { createRequire } from 'module';
 import { fileURLToPath } from 'url';
 import { setupMaster, fork } from 'cluster';
@@ -42,94 +42,6 @@ function start(file) {
 
   setupMaster({ exec: args[0], args: args.slice(1) });
   let p = fork();
-
-  p.on('message', data => {
-    switch (data) {
-      case 'reset':
-        p.kill();
-        isRunning = false;
-        start(file);
-        break;
-      case 'uptime':
-        p.send(process.uptime());
-        break;
-    }
-  });
-
-  p.on('exit', (code) => {
-    isRunning = false;
-    if (code === 0) return;
-    console.error('❌ Error:\n', code);
-    watchFile(args[0], () => {
-      unwatchFile(args[0]);
-      start(file);
-    });
-  });
-
-  const opts = yargs(process.argv.slice(2)).exitProcess(false).parse();
-  if (!opts['test']) {
-    if (!rl.listenerCount('line')) {
-      rl.on('line', line => {
-        p.emit('message', line.trim());
-      });
-    }
-  }
-}
-
-process.on('warning', (warning) => {
-  if (warning.name === 'MaxListenersExceededWarning') {
-    console.warn('🔴 Se excedió el límite de Listeners en:');
-    console.warn(warning.stack);
-  }
-});
-
-start('main.js');*/
-
-import { join, dirname } from 'path';
-import { createRequire } from 'module';
-import { fileURLToPath } from 'url';
-import { fork } from 'child_process'; // ✅ Usamos child_process en lugar de cluster
-import { watchFile, unwatchFile } from 'fs';
-import cfonts from 'cfonts';
-import { createInterface } from 'readline';
-import yargs from 'yargs';
-import chalk from 'chalk';
-
-console.log(`\n💻 Iniciando Sistema`);
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const require = createRequire(__dirname);
-const { name, description, author, version } = require(join(__dirname, './package.json'));
-const { say } = cfonts;
-const rl = createInterface(process.stdin, process.stdout);
-
-say('Jota - MD', {
-  font: 'chrome',
-  align: 'center',
-  gradient: ['red', 'magenta']
-});
-say(`Developed By Shadow's Club 🌹\n&&\nDev Criss 🇦🇱`, {
-  font: 'console',
-  align: 'center',
-  gradient: ['red', 'magenta']
-});
-
-let isRunning = false;
-
-function start(file) {
-  if (isRunning) return;
-  isRunning = true;
-
-  let args = [join(__dirname, file), ...process.argv.slice(2)];
-  say([process.argv[0], ...args].join(' '), {
-    font: 'console',
-    align: 'center',
-    gradient: ['red', 'magenta']
-  });
-
-  let p = fork(args[0], args.slice(1), {
-    stdio: ['inherit', 'inherit', 'inherit', 'ipc'] // Para que funcione p.send()
-  });
 
   p.on('message', data => {
     switch (data) {
