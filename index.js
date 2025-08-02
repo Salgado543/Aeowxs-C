0d277a28b0f636b9320c07ed89933194ac4f3deb
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-08-02 07:28:09 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ import { createInterface } from 'readline';
 import yargs from 'yargs';
 import chalk from 'chalk';
 
-console.log(\n💻 Iniciando Sistema);
+console.log(`\n💻 Iniciando Sistema`);
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const require = createRequire(__dirname);
@@ -104,71 +104,71 @@ const { say } = cfonts;
 const rl = createInterface(process.stdin, process.stdout);
 
 say('Jota - MD', {
-font: 'chrome',
-align: 'center',
-gradient: ['red', 'magenta']
+  font: 'chrome',
+  align: 'center',
+  gradient: ['red', 'magenta']
 });
-say(Developed By Shadow's Club 🌹\n&&\nDev Criss 🇦🇱, {
-font: 'console',
-align: 'center',
-gradient: ['red', 'magenta']
+say(`Developed By Shadow's Club 🌹\n&&\nDev Criss 🇦🇱`, {
+  font: 'console',
+  align: 'center',
+  gradient: ['red', 'magenta']
 });
 
 let isRunning = false;
 
 function start(file) {
-if (isRunning) return;
-isRunning = true;
+  if (isRunning) return;
+  isRunning = true;
 
-let args = [join(__dirname, file), ...process.argv.slice(2)];
-say([process.argv[0], ...args].join(' '), {
-font: 'console',
-align: 'center',
-gradient: ['red', 'magenta']
-});
+  let args = [join(__dirname, file), ...process.argv.slice(2)];
+  say([process.argv[0], ...args].join(' '), {
+    font: 'console',
+    align: 'center',
+    gradient: ['red', 'magenta']
+  });
 
-let p = fork(args[0], args.slice(1), {
-stdio: ['inherit', 'inherit', 'inherit', 'ipc'] // Para que funcione p.send()
-});
+  let p = fork(args[0], args.slice(1), {
+    stdio: ['inherit', 'inherit', 'inherit', 'ipc'] // Para que funcione p.send()
+  });
 
-p.on('message', data => {
-switch (data) {
-case 'reset':
-p.kill();
-isRunning = false;
-start(file);
-break;
-case 'uptime':
-p.send(process.uptime());
-break;
-}
-});
+  p.on('message', data => {
+    switch (data) {
+      case 'reset':
+        p.kill();
+        isRunning = false;
+        start(file);
+        break;
+      case 'uptime':
+        p.send(process.uptime());
+        break;
+    }
+  });
 
-p.on('exit', (code) => {
-isRunning = false;
-if (code === 0) return;
-console.error('❌ Error:\n', code);
-watchFile(args[0], () => {
-unwatchFile(args[0]);
-start(file);
-});
-});
+  p.on('exit', (code) => {
+    isRunning = false;
+    if (code === 0) return;
+    console.error('❌ Error:\n', code);
+    watchFile(args[0], () => {
+      unwatchFile(args[0]);
+      start(file);
+    });
+  });
 
-const opts = yargs(process.argv.slice(2)).exitProcess(false).parse();
-if (!opts['test']) {
-if (!rl.listenerCount('line')) {
-rl.on('line', line => {
-p.emit('message', line.trim());
-});
-}
-}
+  const opts = yargs(process.argv.slice(2)).exitProcess(false).parse();
+  if (!opts['test']) {
+    if (!rl.listenerCount('line')) {
+      rl.on('line', line => {
+        p.emit('message', line.trim());
+      });
+    }
+  }
 }
 
 process.on('warning', (warning) => {
-if (warning.name === 'MaxListenersExceededWarning') {
-console.warn('🔴 Se excedió el límite de Listeners en:');
-console.warn(warning.stack);
-}
+  if (warning.name === 'MaxListenersExceededWarning') {
+    console.warn('🔴 Se excedió el límite de Listeners en:');
+    console.warn(warning.stack);
+  }
 });
 
 start('main.js');
