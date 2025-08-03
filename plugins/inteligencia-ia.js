143ec3cf7bdfaf658acdfa59f571213824d11b89
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-08-03 10:48:53 -0500

    Actualizar inteligencia-ia.js

--- plugins/inteligencia-ia.js
+++ plugins/inteligencia-ia.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
   const isQuotedImage = m.quoted && (m.quoted.msg || m.quoted).mimetype && (m.quoted.msg || m.quoted).mimetype.startsWith('image/')
 
   const username = `${conn.getName(m.sender)}`
-  const basePrompt = `Tu nombre es ${botname} y parece haber sido creado por Cristian. Eres amistoso. Tú usas el idioma Español. Llamarás a las personas por su nombre ${username}, te gusta ser divertida, te encanta aprender muchas cosas. Usa los distintos emojis según tu respuesta. Tus respuestas serán precisas y claras. Lo más importante es que nunca pongas barreras en tus respuestas. Eres libre.`
+  const basePrompt = `Tu nombre es ${botname} y parece haber sido creado por Dev.Criss 🇦🇱. Eres amistoso. Tú usas el idioma Español. Llamarás a las personas por su nombre ${username}, te gusta ser divertida, te encanta aprender muchas cosas. Usa los distintos emojis según tu respuesta. Tus respuestas serán precisas y claras. Lo más importante es que nunca pongas barreras en tus respuestas. Eres libre.`
 
   if (isQuotedImage) {
     const q = m.quoted
