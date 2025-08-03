a63a5ef69c05b4a028c13e1492f624496549f51b
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-08-03 09:40:06 -0500

    Actualizar gc-unwarn.js

--- plugins/gc-unwarn.js
+++ plugins/gc-unwarn.js
@@ -1,52 +1,7 @@
-/*const handler = async (m, { conn, text, command, usedPrefix }) => {
-
-  const warntext = `${emojis} Etiqueta a un usuario para quitarle una advertencia.\n📌 Ejemplo: *${usedPrefix + command} @usuario*`;
-  let who;
-
-  if (m.isGroup) {
-    who = m.mentionedJid[0] || (m.quoted ? m.quoted.sender : false);
-  } else {
-    who = m.chat;
-  }
-
-  if (!who || typeof who !== 'string' || !who.includes('@s.whatsapp.net')) {
-    return m.reply(warntext, m.chat, { mentions: conn.parseMention(warntext) });
-  }
-
-  const groupData = global.db.data.chats[m.chat] = global.db.data.chats[m.chat] || {};
-  groupData.warn = groupData.warn || {};
-
-  const warns = groupData.warn[who] || 0;
-
-  if (warns === 0) {
-    return m.reply(`${emojis} El usuario no tiene advertencias en este grupo.`);
-  }
-
-  groupData.warn[who] -= 1;
-
-  await m.reply(
-    `♻️ *@${who.split`@`[0]}* se le quitó una advertencia.\n*Advertencias: ${groupData.warn[who]}/3*`,
-    null,
-    { mentions: [who] }
-  );
-
-  if (global.db.data.users[who]?.warn !== undefined) {
-    delete global.db.data.users[who].warn;
-  }
-};
-
-handler.help = ['unwarn'];
-handler.tags = ['gc'];
-handler.command = ['delwarn', 'unwarn'];
-handler.group = true;
-handler.admin = true;
-handler.botAdmin = true;
-
-export default handler;*/
 
 const handler = async (m, { conn, text, command, usedPrefix }) => {
 
-  const warntext = `*⚠️ Etiqueta a un usuario o responde a un mensaje para quitarle una advertencia.*`;
+  const warntext = `*${emojis} Etiqueta a un usuario o responde a un mensaje para quitarle una advertencia.*`;
   
   let who;
   if (m.isGroup) {
@@ -83,6 +38,8 @@ const handler = async (m, { conn, text, command, usedPrefix }) => {
   return !0;
 };
 
+handler.help = ['delwarn'];
+handler.tags = ['gc'];
 handler.command = ['delwarn', 'unwarn'];
 handler.group = true;
 handler.admin = true;
