0bf836d81c4f001bbf2261ce0e7e8a7e24f61f6a
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-08-03 06:03:09 -0500

    Actualizar gc-admins.js

--- plugins/gc-admins.js
+++ plugins/gc-admins.js
@@ -1,20 +1,20 @@
 const handler = async (m, {conn, participants, groupMetadata, args}) => {
-  const pp = await conn.profilePictureUrl(m.chat, 'image').catch((_) => null) || './media/menus/Menu3.jpg';
+  const pp = await conn.profilePictureUrl(m.chat, 'image').catch((_) => null) || './src/catalogo.jpg';
   const groupAdmins = participants.filter((p) => p.admin);
   const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n');
   const owner = groupMetadata.owner || groupAdmins.find((p) => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net';
   const pesan = args.join` `;
-  const oi = `» ${pesan}`;
-  const text = `🎩 Invocando Admins:  
+  const oi = `${pesan}`;
+  const text = `*${xgc} Invocando Admins*
   
 ${listAdmin}
 
-🍭 Mensaje: ${oi}
+𖡻፞ 🍄 Mensaje: ${oi}
 
-『✦』Este comando solo se puede ejecutar si tienes algún problema o duda con el *Bot*, si lo usas con otras intenciones se te *baneara* o *eliminara* del grupo.`.trim();
+> ✿ Este comando solo puede ser ejecutado si hay algún problema en el grupo o con el bot.`.trim();
   conn.sendFile(m.chat, pp, 'error.jpg', text, m, false, {mentions: [...groupAdmins.map((v) => v.id), owner]});
 };
-handler.help = ['admins *<texto>*'];
+handler.help = ['admins'];
 handler.tags = ['gc'];
 handler.customPrefix = /a|@|./i;
 handler.command = /^(admins|@admins|dmins)$/i;
