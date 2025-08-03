444c88ab99ff11679c1ebe6b68e30e0000071949
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-08-03 09:48:07 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ say('Jota - MD', {
   align: 'center',
   gradient: ['red', 'magenta']
 });
-say(`Developed By Shadow's Club 🌹\nSi viste esto es xk funciona 😛\nby el tío Cris un crack 😎`, {
+say(`Developed By Shadow's Club 🌹\n&&\nDev Criss 🇦🇱`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']
