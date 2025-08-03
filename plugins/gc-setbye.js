dc2bedde7de0893109e2022199d4bdd47d9ae35c
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-08-03 08:31:24 -0500

    Actualizar gc-setbye.js

--- plugins/gc-setbye.js
+++ plugins/gc-setbye.js
@@ -5,7 +5,7 @@ global.db.data.chats[m.chat].sBye = text
 conn.reply(m.chat, `*✅ La Despedida del grupo ha sido configurada*`, m, rcanal)  
 
 } else {
-    conn.reply(m.chat, `*${emojis} ¡Escribe el mensaje de despedida!*\n✎ *Puedes usar:*\n\n- *\`@user\`* (Mención al Usuario)\n- *\`@group\`* (Nombre del Grupo)\n- *\`@desc\`* (Descripción del Grupo)\n\n> ${emojis} Los @ son opcionales`, m, rcanal)
+    conn.reply(m.chat, `*${emojis} ¡Escribe el mensaje de despedida!*\n✎ *Puedes usar:*\n\n- *\`@user\`* (Mención al Usuario)\n- *\`@group\`* (Nombre del Grupo)\n- *\`@desc\`* (Descripción del Grupo)\n\n> ${emojis} Los @ son opcionales`, m)
 }
 }
 
