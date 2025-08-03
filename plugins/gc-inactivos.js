12a04287d462552697aed307cec338417ad9e700
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-08-03 08:10:54 -0500

    Actualizar gc-inactivos.js

--- plugins/gc-inactivos.js
+++ plugins/gc-inactivos.js
@@ -56,7 +56,7 @@ let handler = async (m, { conn, text, args, groupMetadata }) => {
         return conn.reply(m.chat, `*${emoji} Eliminado con éxito ${total} miembros inactivos del grupo.*`, m)
     }
 
-    return conn.reply(m.chat, `*${emojis} La opción no es válida*.\n> *Utilice:*\n\`list\` para ver miembros inactivos\n\`kick\` para eliminarlos.`, m)
+    return conn.reply(m.chat, `*${emojis} La opción no es válida*.\n> *Utilice:*\n> \`list\` para ver miembros inactivos\n> \`kick\` para eliminarlos.`, m)
 }
 
 handler.help = ['inactivos list', 'inactivos kick']
