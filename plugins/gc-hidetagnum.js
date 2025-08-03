82b926b100ebe6817c0b299acf562ad06ddfc7d8
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-08-03 07:15:27 -0500

    Actualizar gc-hidetagnum.js

--- plugins/gc-hidetagnum.js
+++ plugins/gc-hidetagnum.js
@@ -13,7 +13,7 @@ const handler = async (m, { conn, args, participants, usedPrefix, command }) => 
   if (usuariosConPrefijo.length === 0) 
     return m.reply(`*${emojis} No hay ningún número con el prefijo \`+${prefijo}\` en este grupo.*`);
 
-  const listaUsuarios = usuariosConPrefijo.map((v) => '┋💙 @' + v.replace(/@.+/, '')).join('\n');
+  const listaUsuarios = usuariosConPrefijo.map((v) => '${emotg} @' + v.replace(/@.+/, '')).join('\n');
 
   conn.reply(
     m.chat, 
@@ -23,7 +23,7 @@ const handler = async (m, { conn, args, participants, usedPrefix, command }) => 
   );
 };
 
-handler.help = ['tagnum *<prefix>*'];
+handler.help = ['tagnum'];
 handler.command = /^(tagnum|hidetagnum)$/i;
 handler.group = true;
 handler.admin = true;
