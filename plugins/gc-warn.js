babe2db0ac4fbe376eb5018ceeb0a73ab3880ba9
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-08-03 10:11:07 -0500

    Actualizar gc-warn.js

--- plugins/gc-warn.js
+++ plugins/gc-warn.js
@@ -52,7 +52,7 @@ const handler = async (m, { conn, text, command, usedPrefix }) => {
   if (chat.warns[who] >= 3) {
     chat.warns[who] = 0;
     await m.reply(
-      `❗ *Advertencia final* ❗.\n\n*@${who.split`@`[0]}* superó las 3 advertencias y será eliminado.`,
+      `❗ *Advertencia final* ❗.\n\n*🥥 @${who.split`@`[0]}* superó las 3 advertencias y será eliminado.`,
       null,
       { mentions: [who] }
     );
