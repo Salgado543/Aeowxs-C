ed29538142406aca7926d4f9d7e02d5a9779711a
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-08-04 01:53:25 -0500

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,51 +1,34 @@
 import fetch from 'node-fetch';
 
 let handler = async (m, { conn, usedPrefix, text, args, command }) => {
-    await m.react('🤍');
+   await m.react('☕');
 
     let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
     let name = await conn.getName(who);
-    let edtr = `@${m.sender.split`@`[0]}`;
-    let username = await conn.getName(m.sender);
-
-    // Descargar imagen como buffer
-    let res = await fetch('https://files.catbox.moe/8an9fs.jpg');
-    let thumb = await res.buffer();
+    let username = conn.getName(m.sender);
 
     // VCARD
     let list = [{
         displayName: "Cristian Escobar",
-        vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:Cristian Escobar\nitem1.TEL;waid=51927238856:51927238856\nitem1.X-ABLabel:Número\nitem2.EMAIL;type=INTERNET:cristianescobar.vx@gmail.com\nitem2.X-ABLabel:Email\nitem3.URL:https://www.instagram.com/dev.criss_vx\nitem3.X-ABLabel:Instagram\nitem4.ADR:;;Perú;;;;\nitem4.X-ABLabel:País\nEND:VCARD`
+        vcard: `BEGIN:VCARD\nVERSION:3.0\nFN: Cristian Escobar
+\nitem1.TEL;waid=51927238856:51927238856\nitem1.X-ABLabel:Número\nitem2.EMAIL;type=INTERNET: cristianescobar.vx@gmail.com\nitem2.X-ABLabel:Email\nitem3.URL:https://www.instagram.com/dev.criss_vx\nitem3.X-ABLabel:Internet\nitem4.ADR:;; Perú 🇵🇪;;;;\nitem4.X-ABLabel:Region\nEND:VCARD`,
     }];
 
-    // Enviar contacto con vista previa enriquecida
     await conn.sendMessage(m.chat, {
         contacts: {
             displayName: `${list.length} Contacto`,
             contacts: list
-        },
-        contextInfo: {
-            externalAdReply: {
-                showAdAttribution: true,
-                title: 'Hello I am the creator of Jota',
-                body: 'Dev.Criss 🇦🇱',
-                thumbnail: thumb,
-                sourceUrl: 'https://wa.me/51927238856?text=¡Hola!,+Quiero+bot+perzonalizando.',
-                mediaType: 1,
-                renderLargerThumbnail: true
-            }
         }
     }, {
         quoted: m
     });
 
-    // Mensaje de texto adicional
-    let txt = `👋🏻 *Hola \`${username}\` este es*\n*el contacto de mi creador*`;
+    let txt = `👋 *Hola \`${username}\` este es*\n*el contacto de mi desarrollador*`;
 
     await conn.sendMessage(m.chat, { text: txt }, { quoted: m });
 };
 
-handler.help = ['owner', 'creador'];
+handler.help = ['creador'];
 handler.tags = ['info'];
 handler.command = /^(creator|creador)$/i;
 
