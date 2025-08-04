2da55e647325cc70fdeb141393764d449b780cc5
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-08-04 02:04:54 -0500

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,35 +1,29 @@
-import fetch from 'node-fetch';
+import fetch from 'node-fetch'
 
-let handler = async (m, { conn, usedPrefix, text, args, command }) => {
-   await m.react('☕');
+let handler = async (m, { conn }) => {
+  await m.react(emojis);
 
-    let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
-    let name = await conn.getName(who);
-    let username = conn.getName(m.sender);
+  let list = [
+    {
+      displayName: "Cristian Escobar",
+      vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:Cristian Escobar\nitem1.TEL;waid=51927238856:51927238856\nitem1.X-ABLabel:Número\nitem2.EMAIL;type=INTERNET:cristianescobar.vx@gmail.com\nitem2.X-ABLabel:Email\nitem3.URL:https://www.instagram.com/dev.criss_vx\nitem3.X-ABLabel:Instagram\nitem4.ADR:;;Perú 🇵🇪;;;;\nitem4.X-ABLabel:País\nEND:VCARD`
+    },
+    {
+      displayName: "Jota",
+      vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:Jota\nitem1.TEL;waid=573155227977:573155227977\nitem1.X-ABLabel:Número\nitem2.EMAIL;type=INTERNET:team.sunflare@gmail.com\nitem2.X-ABLabel:Email\nitem3.URL:https://www.instagram.com/nee\nitem3.X-ABLabel:Instagram\nitem4.ADR:;;Colombia 🇨🇴;;;;\nitem4.X-ABLabel:País\nEND:VCARD`
+    }
+  ];
 
-    // VCARD
-    let list = [{
-        displayName: "Cristian Escobar",
-        vcard: `BEGIN:VCARD\nVERSION:3.0\nFN: Cristian Escobar
-\nitem1.TEL;waid=51927238856:51927238856\nitem1.X-ABLabel:Número\nitem2.EMAIL;type=INTERNET: cristianescobar.vx@gmail.com\nitem2.X-ABLabel:Email\nitem3.URL:https://www.instagram.com/dev.criss_vx\nitem3.X-ABLabel:Internet\nitem4.ADR:;; Perú 🇵🇪;;;;\nitem4.X-ABLabel:Region\nEND:VCARD`,
-    }];
-
-    await conn.sendMessage(m.chat, {
-        contacts: {
-            displayName: `${list.length} Contacto`,
-            contacts: list
-        }
-    }, {
-        quoted: m
-    });
-
-    let txt = `👋 *Hola \`${username}\` este es*\n*el contacto de mi desarrollador*`;
-
-    await conn.sendMessage(m.chat, { text: txt }, { quoted: m });
+  await conn.sendMessage(m.chat, {
+    contacts: {
+      displayName: `${list.length} Contactos`,
+      contacts: list
+    }
+  }, { quoted: estilo });
 };
 
-handler.help = ['creador'];
+handler.help = ['creador', 'dueño'];
 handler.tags = ['info'];
-handler.command = /^(creator|creador)$/i;
+handler.command = /^(owner|creador|creator|dueño|desarrollador)$/i;
 
 export default handler;
