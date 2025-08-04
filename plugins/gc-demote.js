33cb719a6ba8f467429118f7d47d0991a3da2d12
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-08-04 15:02:53 -0500

    Actualizar gc-demote.js

--- plugins/gc-demote.js
+++ plugins/gc-demote.js
@@ -1,7 +1,7 @@
 /* 
 Created by Crxstian Escobar 🌙
 */
-
+/*
 const handler = async (m, {conn, usedPrefix, text}) => {
   if (isNaN(text) && !text.match(/@/g)) {
   } else if (isNaN(text)) {
@@ -37,4 +37,41 @@ handler.group = true;
 handler.admin = true;
 handler.botAdmin = true;
 handler.fail = null;
+export default handler;*/
+
+
+const handler = async (m, { conn, text }) => {
+  if (!text && !m.quoted && !m.mentionedJid.length) {
+    return conn.reply(m.chat, `*${emojis} Menciona o responde a un usuario para quitar admin.*`, m);
+  }
+
+  let user;
+  if (m.mentionedJid[0]) {
+    user = m.mentionedJid[0];
+  } else if (m.quoted) {
+    user = m.quoted.sender;
+  } else if (text) {
+    const number = text.replace(/\D/g, '');
+    if (number.length < 11 || number.length > 13) {
+      return conn.reply(m.chat, `*⚠️ El número ingresado es incorrecto.*`, m);
+    }
+    user = number + '@s.whatsapp.net';
+  }
+
+  const groupMetadata = await conn.groupMetadata(m.chat);
+  if (user === groupMetadata.owner) {
+    return conn.reply(m.chat, `*⚠️ No se puede degradar al creador del grupo.*`, m);
+  }
+
+  await conn.groupParticipantsUpdate(m.chat, [user], 'demote');
+  return conn.reply(m.chat, `*✅ El usuario fue degradado de la administración.*`, m);
+};
+
+handler.help = ['demote'];
+handler.tags = ['gc'];
+handler.command = /^(demote|quitarpoder|quitaradmin)$/i;
+handler.group = true;
+handler.admin = true;
+handler.botAdmin = true;
+
 export default handler;
