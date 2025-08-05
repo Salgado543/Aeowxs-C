476d451245c694cbbfdc673bac37df4fd33d5a0c
𝙎𝙝𝙖𝙙𝙤𝙬'𝙨 𝘾𝙡𝙪𝙗 𝙊𝙛𝙞𝙘𝙞𝙖𝙡 🌹 <shadows.club.vx@gmail.com>
2025-08-05 09:09:51 -0500

    Actualizar main-menunsfw.js

--- plugins/main-menunsfw.js
+++ plugins/main-menunsfw.js
@@ -2,7 +2,7 @@
 let handler = async (m, { conn, usedPrefix: _p }) => {
 
   let usertag = '@' + m.sender.split('@')[0]
-  const img = 'https://files.catbox.moe/povyqs.jpg'
+  const img = 'https://files.catbox.moe/vjyp4o.jpg'
 
   let tags = {
     "xsearch": "「 *Search* 」🍄",
