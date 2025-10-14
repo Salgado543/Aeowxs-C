f29da865fad68f6298dfb525c278e0f9d1f460ad
🤍 ꪶ 𝐒𝐡֟፝𝐚𝐝𝐨𝐰'𝐬  𝐂𝐥𝐮𝐛 ּꫂ   ֪ 稜 <shadows.club.vx@gmail.com>
2025-10-13 22:37:44 -0500

    Actualizar config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 import moment from 'moment-timezone' 
 
-global.botNumber = '+15072507631' //Ejemplo: 51927238856
+global.botNumber = '' //Ejemplo: 51927238856
 
 global.owner = [
    ['906168999', 'Cielo', true],
