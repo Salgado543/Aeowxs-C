629dea2f35894bf9f7146a68965f24ab5805ab38
🤍 ꪶ 𝐒𝐡֟፝𝐚𝐝𝐨𝐰'𝐬  𝐂𝐥𝐮𝐛 ּꫂ   ֪ 稜 <shadows.club.vx@gmail.com>
2025-10-14 21:25:42 -0500

    Actualizar sticker-qc.js

--- plugins/sticker-qc.js
+++ plugins/sticker-qc.js
@@ -17,7 +17,7 @@ const nombre = await conn.getName(who)
 const obj = {"type": "quote", "format": "png", "backgroundColor": "#000000", "width": 512, "height": 768, "scale": 2, "messages": [{"entities": [], "avatar": true, "from": {"id": 1, "name": usname, "photo": {url: `${pp}`}}, "text": mishi, "replyMessage": {}}]};
 const json = await axios.post('https://bot.lyo.su/quote/generate', obj, {headers: {'Content-Type': 'application/json'}});
 const buffer = Buffer.from(json.data.result.image, 'base64');
-let stiker = await sticker(buffer, false, global.packname, global.author);
+let stiker = await sticker(buffer, false, global.packN, global.authN);
 if (stiker) return conn.sendFile(m.chat, stiker, 'error.webp', '', m);
 }
 handler.help = ['qc'];
