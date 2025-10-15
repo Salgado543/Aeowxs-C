b355e71423617cd789ae60e511826dec064cb3aa
🤍 ꪶ 𝐒𝐡֟፝𝐚𝐝𝐨𝐰'𝐬  𝐂𝐥𝐮𝐛 ּꫂ   ֪ 稜 <shadows.club.vx@gmail.com>
2025-10-14 21:31:37 -0500

    Actualizar owner-unbanchat.js

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -3,7 +3,7 @@ if (!(m.chat in global.db.data.chats)) return conn.reply(m.chat, '*⚠️ Este c
 let chat = global.db.data.chats[m.chat]
 if (!chat.isBanned) return conn.reply(m.chat, '*⚠️ Este chat no esta baneado*', m)
 chat.isBanned = false
-await conn.reply(m.chat, `*${emojis} Chat desbaneado.*`, m)
+await conn.reply(m.chat, `*${emojis} Este chat fue desbaneado*`, m)
 }
 handler.help = ['unbanchat'];
 handler.tags = ['owner'];
