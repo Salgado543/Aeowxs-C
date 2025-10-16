c5f25c2639ec6b2370cb876d61ca1f57d84f9213
🤍 ꪶ 𝐒𝐡֟፝𝐚𝐝𝐨𝐰'𝐬  𝐂𝐥𝐮𝐛 ּꫂ   ֪ 稜 <shadows.club.vx@gmail.com>
2025-10-15 19:41:22 -0500

    Actualizar fun-personalidad.js

--- plugins/fun-personalidad.js
+++ plugins/fun-personalidad.js
@@ -1,31 +1,44 @@
-let handler = async (m, { conn, command, text }) => {
+let handler = async (m, { conn, usedPrefix, command, text }) => {
+  let user = m.mentionedJid && m.mentionedJid[0]
+           ? m.mentionedJid[0]
+           : m.quoted?.sender
+             ? m.quoted.sender
+             : null;
 
-    if (!text) return conn.reply(m.chat, `*${emojis} Ingrese el nombre de alguna persona.*\n> *Ejemplo:* .personalidad ${namebot}`, m, rcanal)
+  if (!user) {
+    return conn.reply(m.chat, `*${emojis} Ingresa el nombre o menciona a alguien.*`, m);
+  }
 
-  let personalidad = `
-┏━━[ *PERSONALIDAD* ]━━┓
-*┃*
-*┃• Nombre* : ${text}
-*┃• Buena Moral* : ${pickRandom(['6%','12%','20%','27%','35%','41%','49%','54%','60%','66%','73%','78%','84%','92%','93%','94%','96%','98,3%','99,7%','99,9%','1%','2,9%','0%','0,4%'])}
-*┃• Mala Moral* : ${pickRandom(['6%','12%','20%','27%','35%','41%','49%','54%','60%','66%','73%','78%','84%','92%','93%','94%','96%','98,3%','99,7%','99,9%','1%','2,9%','0%','0,4%'])}
-*┃• Tipo de persona* : ${pickRandom(['De buen corazón','Arrogante','Tacaño','Generoso','Humilde','Tímido','Cobarde','Entrometido','Cristal','No binarie XD', 'Pendejo'])}
-*┃• Siempre* : ${pickRandom(['Pesado','De malas','Distraido','De molestoso','Chismoso','Pasa jalandosela','De compras','Viendo anime','Chatea en WhatsApp porque esta soltero','Acostado bueno para nada','De mujeriego','En el celular'])}
-*┃• Inteligencia* : ${pickRandom(['6%','12%','20%','27%','35%','41%','49%','54%','60%','66%','73%','78%','84%','92%','93%','94%','96%','98,3%','99,7%','99,9%','1%','2,9%','0%','0,4%'])}
-*┃• Morosidad* : ${pickRandom(['6%','12%','20%','27%','35%','41%','49%','54%','60%','66%','73%','78%','84%','92%','93%','94%','96%','98,3%','99,7%','99,9%','1%','2,9%','0%','0,4%'])}
-*┃• Coraje* : ${pickRandom(['6%','12%','20%','27%','35%','41%','49%','54%','60%','66%','73%','78%','84%','92%','93%','94%','96%','98,3%','99,7%','99,9%','1%','2,9%','0%','0,4%'])}
-*┃• Miedo* : ${pickRandom(['6%','12%','20%','27%','35%','41%','49%','54%','60%','66%','73%','78%','84%','92%','93%','94%','96%','98,3%','99,7%','99,9%','1%','2,9%','0%','0,4%'])}
-*┃• Fama* : ${pickRandom(['6%','12%','20%','27%','35%','41%','49%','54%','60%','66%','73%','78%','84%','92%','93%','94%','96%','98,3%','99,7%','99,9%','1%','2,9%','0%','0,4%'])}
-*┃• Género* : ${pickRandom(['Hombre', 'Mujer', 'Homosexual', 'Bisexual', 'Pansexual', 'Feminista', 'Heterosexual', 'Macho alfa', 'Mujerzona', 'Marimacha', 'Palosexual', 'PlayStationSexual', 'Sr. Manuela', 'Pollosexual'])}
-┗━━━━━━━━━━━━━━━━
-`
-conn.reply(m.chat, personalidad, m, { mentions: conn.parseMention(personalidad) })
-}
-handler.help = ['personalidad']
-handler.tags = ['fun']
-handler.command = /^personalidad/i
+  const taguser = '@' + user.split('@')[0];
 
-export default handler 
+  const personalidad = `
+*╭╼━•♡ Personalidad ♡•━─*
+*│ 💫 Nombre:* ${taguser}
+*│ 🙂 Buena moral:* ${pickRandom(['6%','12%','20%','27%','35%','41%','49%','54%','60%','66%','73%','78%','84%','92%','93%','94%','96%','98,3%','99,7%','99,9%','1%','2,9%','0%','0,4%'])}
+*│ 🙄 Mala moral:* ${pickRandom(['6%','12%','20%','27%','35%','41%','49%','54%','60%','66%','73%','78%','84%','92%','93%','94%','96%','98,3%','99,7%','99,9%','1%','2,9%','0%','0,4%'])}
+*│ 💟 Tipo de persona:* ${pickRandom(['De buen corazón','Arrogante','Tacaño','Generoso','Humilde','Tímido','Cobarde','Entrometido','Cristal','No binarie XD', 'Pendejo'])}
+*│ 🪶 Siempre:* ${pickRandom(['Pesado','De malas','Distraido','De molestoso','Chismoso','Pasa jalandosela','De compras','Viendo anime','Chatea en WhatsApp porque esta soltero','Acostado bueno para nada','De mujeriego','En el celular'])}
+*│ 🤓 Inteligencia:* ${pickRandom(['6%','12%','20%','27%','35%','41%','49%','54%','60%','66%','73%','78%','84%','92%','93%','94%','96%','98,3%','99,7%','99,9%','1%','2,9%','0%','0,4%'])}
+*│ 🤯 Morosidad:* ${pickRandom(['6%','12%','20%','27%','35%','41%','49%','54%','60%','66%','73%','78%','84%','92%','93%','94%','96%','98,3%','99,7%','99,9%','1%','2,9%','0%','0,4%'])}
+*│ 💪🏻 Coraje:* ${pickRandom(['6%','12%','20%','27%','35%','41%','49%','54%','60%','66%','73%','78%','84%','92%','93%','94%','96%','98,3%','99,7%','99,9%','1%','2,9%','0%','0,4%'])}
+*│ 😨 Miedo:* ${pickRandom(['6%','12%','20%','27%','35%','41%','49%','54%','60%','66%','73%','78%','84%','92%','93%','94%','96%','98,3%','99,7%','99,9%','1%','2,9%','0%','0,4%'])}
+*│ 🌟 Fama:* ${pickRandom(['6%','12%','20%','27%','35%','41%','49%','54%','60%','66%','73%','78%','84%','92%','93%','94%','96%','98,3%','99,7%','99,9%','1%','2,9%','0%','0,4%'])}
+*│ 🚻 Género:* ${pickRandom(['Hombre', 'Mujer', 'Homosexual', 'Bisexual', 'Pansexual', 'Feminista', 'Heterosexual', 'Macho alfa', 'Mujerzona', 'Marimacha', 'Palosexual', 'Kbro', 'Sr. Manuela', 'Pollosexual', 'Gei', 'Lesbi', 'Trans', 'No sabe que es :v'])}
+*╰╼━━━━━━━━━━━─⪩*
+`;
+
+  await conn.sendMessage(m.chat, {
+    text: personalidad,
+    mentions: [user]
+  });
+};
+
+handler.help = ['personalidad'];
+handler.tags = ['fun'];
+handler.command = /^personalidad/i;
+
+export default handler;
 
 function pickRandom(list) {
-  return list[Math.floor(Math.random() * list.length)]
+  return list[Math.floor(Math.random() * list.length)];
 }
