9d49c436ff2279b1eabb97ba40bc0c3e9d99b54e
🤍 ꪶ 𝐒𝐡֟፝𝐚𝐝𝐨𝐰'𝐬  𝐂𝐥𝐮𝐛 ּꫂ   ֪ 稜 <shadows.club.vx@gmail.com>
2025-10-15 22:28:30 -0500

    Actualizar owner-restart.js

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -1,23 +1,11 @@
-/*
-const handler = async (m, {conn, isROwner, text}) => {
-  if (!process.send) throw 'Dont: node main.js\nDo: node index.js';
-    // conn.readMessages([m.key])
-    await m.reply('*🔄 Reiniciando el bot...*\n*» Espere un momento para volver a usar el Bot, puede tomar unos segundos.*');
-    process.send('reset');
-};
-handler.help = ['restart'];
-handler.tags = ['owner'];
-handler.command = ['restart', 'reiniciar'];
-handler.rowner = true;
-export default handler;
-*/
+
 const handler = async (m, { conn }) => {
-  await m.reply('*☕ Reiniciando el bot.*\n\n> Espere unos segundos...');
-  
-  if (process.send) {
-    process.send('reset');
-  } else {
-    setTimeout(() => process.exit(0), 3000);
+  try {
+    await m.reply(`☕ *Reiniciando el sistema...*\n> Espera un momento para finalizar.`);
+    setTimeout(() => process.exit(0), 3000); // sale del proceso en 3 segundos
+  } catch (err) {
+    console.error(err);
+    conn.reply(m.chat, `${err}`, m);
   }
 };
 
